bb0691edb50756e49882db211da3ebeb79933104
Anar <153716590+Anar0204@users.noreply.github.com>
2025-05-20 18:31:46 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/ExtensionMethods.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class ExtensionMethods
+    {
+        
+        public static bool Tek( int number)
+        {
+            return number % 2 != 0;
+        }
+
+        public static bool Cut( int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static bool IsContainsdigit(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] >= '0' && input[i] <= '9')
+                    return true;
+            }
+            return false;
+        }
+
+        public static string Capitalize(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return input;
+            }
+                
+
+            return char.ToUpper(input[0]) + input.Substring(1).ToLower();
+        }
+
+        public static int[] GetValueIndexes(string input, char target)
+        {
+            if (string.IsNullOrEmpty(input))
+                return new int[0];
+
+           
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == target)
+                    count++;
+            }
+
+         
+            int[] result = new int[count];
+            int index = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == target)
+                {
+                    result[index] = i;
+                    index++;
+                }
+            }
+
+            return result;
+        }
+
+        public static string GetFirstSentence(string input)
+        {
+
+            int index = input.IndexOf('.');
+            if (index == -1)
+            {
+                return input.Trim();
+            }
+             
+
+            return input.Substring(0, index + 1).Trim();
+        }
+
+        public static string GetSecondWord(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return string.Empty;
+
+            
+            string[] words = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length >= 2 ? words[1] : string.Empty;
+        }
+    }
+}
